e477f6ebb063d766f9487772626d1ce16ffa1c74
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-06-11 14:13:06 -0500

    created searchableList component and added debouncing to onChange

---
+++ src/components/SearchableList/SearchableList.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+const SearchableList = ({ items, itemKeyFn, children }) => {
+  const lastChange=useRef();
+  const [searchTerm, setSearchTerm] = useState("");
+  const searchResults = items.filter((item) =>
+    JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  function handleChange(event) {
+    if(lastChange.current) {
+      clearTimeout(lastChange.current)
+    }
+
+
+    lastChange.current= setTimeout(()=>{
+      lastChange.current = null
+      setSearchTerm(event.target.value)}, 500);
+    ;
+  }
+
+  return (
+    <div className="searchable-lost">
+      <input type="search" placeholder="Search" onChange={handleChange} />
+      <ul>
+        {searchResults.map((item) => (
+          <li key={itemKeyFn(item)}>{children(item)}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SearchableList;
